eb1a3363117547fa5c0a4c5bb632a3e8aff0f119
Jackson Gray <nohaxjustdoge@gmail.com>
2024-02-21 16:49:33 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    const result = record.find(function(game) {
+        return game.result === "W";
+    });
+    return result ? result.year : undefined;
+}
+
+
